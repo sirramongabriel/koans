3239555a953b4132380f01b8fbbbad7203813a04
sirramongabriel <sirramongabriel@gmail.com>
2013-11-10 18:54:40 -0500

    Finish switch statement for score var

--- greed/greed.rb
+++ greed/greed.rb
@@ -2,24 +2,22 @@ class Greed
   die = [1, 2, 3, 4, 5, 6]
   roll = [die.sample, die.sample, die.sample, die.sample, die.sample]
 
-  case roll
-  when roll.take_while { |num| num == 3 }
+  case score
+  when roll.count { |num| num == 1 }.eql? 3
     return 1000
-  when roll & [6, 6, 6]
+  when roll.count { |num| num == 6 }.eql? 3
     return 600
-  when roll & [5, 5, 5]
+  when roll.count { |num| num == 5 }.eql? 3
     return 500
-  when roll & [4, 4, 4]
+  when roll.count { |num| num == 4 }.eql? 3
     return 400
-  when roll & [3, 3, 3]
+  when roll.count { |num| num == 3 }.eql? 3
     return 300
-  when roll & [2, 2, 2]
+  when roll.count { |num| num == 2 }.eql? 3
     return 200
   when roll.include?(1)
     return 100
   else
     return 50
   end
-
-  roll.keep_if { |n| n == 3} == 3
 end
